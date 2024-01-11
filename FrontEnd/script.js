1794db5d58693bd2d621eeee5e5fe343cc9f2ae9
MUNTEAN ANASTASIA <mun.anastasia99@gmail.com>
2024-01-11 01:22:53 +0100

    aficher la gallerie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,26 +1,25 @@
-
-
 async function getworks() {
     const  reponse = await fetch(`http://localhost:5678/api/works`);
     const works = await reponse.json();
     console.log(works);
 
-    /*const sectionGallery = document.querySelector(".gallery");
-    const figure = document.createElement("figure");
-    sectionGallery.appendChild(figure);*/
-    const image = document.createElement("img");
-    for(let i = 0; i < image.lenght; i++){
+
+    works.forEach(works => {
         const sectionGallery = document.querySelector(".gallery");//je rentre dans la balise gallery
         const figure = document.createElement("figure");// je cree nouvelle balise figure 
-        sectionGallery.appendChild(figure);
-        const image = document.createElement("img");
-        figure.appendChild(image);
+        const imageElement = document.createElement("img");
+        imageElement.src = works.imageUrl;
         const figcaption = document.createElement("figcation");
-        figcaption.innerHTML = "Abajour Tahina";
-        figure.appendChild(figcaption);}
-    //image.src="http://localhost:5678/images/abajour-tahina1651286843956.png";
-    //figure.appendChild(image);
+        figcaption.innerHTML = works.title;
+
+   
+        sectionGallery.appendChild(figure);
+        figure.appendChild(imageElement);
+        figure.appendChild(figcaption);
     
+    });
 }
 getworks();
+
+
 

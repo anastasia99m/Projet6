cddfd3b6a33d5d11ba4a14529aa033887922805b
MUNTEAN ANASTASIA <mun.anastasia99@gmail.com>
2024-01-11 01:52:25 +0100

    ajout-btn-filtres-js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,10 +3,17 @@ async function getworks() {
     const works = await reponse.json();
     console.log(works);
 
+    const getCategories = await fetch("http://localhost:5678/api/categories");
+    const categories = await getCategories.json();
+    console.log(categories);
 
+    
+    const sectionGallery = document.querySelector(".gallery");//Rentrer dans la balise gallery
+    sectionGallery.innerHTML = ""; //Vider le contenu de la gallery existante
+
+    // Afficher la gallerie
     works.forEach(works => {
-        const sectionGallery = document.querySelector(".gallery");//je rentre dans la balise gallery
-        const figure = document.createElement("figure");// je cree nouvelle balise figure 
+        const figure = document.createElement("figure");// Creer nouvelle balise figure 
         const imageElement = document.createElement("img");
         imageElement.src = works.imageUrl;
         const figcaption = document.createElement("figcation");
@@ -16,8 +23,23 @@ async function getworks() {
         sectionGallery.appendChild(figure);
         figure.appendChild(imageElement);
         figure.appendChild(figcaption);
-    
     });
+
+    // Creer les buttons de filtres
+    const filtres = document.querySelector(".filtres");
+    // Le button Tous
+    const btnTous = document.createElement("button");
+      btnTous.textContent = "Tous";
+      filtres.appendChild(btnTous);
+    // Les buttons des categories
+    categories.forEach((category) => {
+        const btn = document.createElement("button");
+        btn.textContent = category.name;
+        btn.id = category.id;
+        filtres.appendChild(btn);
+      });
+
+      
 }
 getworks();
 
